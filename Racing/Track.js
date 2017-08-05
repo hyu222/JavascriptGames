81b5cfcbc6bf2d67a30062556bfde726165e820b
Haitian Yu <hyu222@uwo.ca>
2017-08-05 14:46:47 -0400

    Consolidated image loading and added code to not allow game to start until all images are loaded. Also added a very basic loading screen

--- Racing/Track.js
+++ Racing/Track.js
@@ -1,6 +1,3 @@
-var roadPic = document.createElement("img");
-var wallPic = document.createElement("img");
-
 const TRACK_W = 40;
 const TRACK_H = 40;
 const TRACK_GAP = 2;
@@ -25,11 +22,6 @@ var trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
 const TRACK_PLAYERSTART = 2;
-
-function trackLoadImages() {
-	roadPic.src = "track_road.png";
-	wallPic.src = "track_wall.png";
-}
 
 function isWallAtColRow(col, row) {
 	if(col >= 0 && col < TRACK_COLS &&
