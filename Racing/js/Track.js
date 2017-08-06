47bc96f33575e661993f8c3b8c6af5407e7fa0d9
Haitian Yu <hyu222@uwo.ca>
2017-08-06 16:27:22 -0400

    Created a car class and created player 2 car with same functionality as player 1 car.

--- Racing/js/Track.js
+++ Racing/js/Track.js
@@ -13,7 +13,7 @@ var trackGrid = [4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4,
 				 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 5, 0, 0, 1, 0, 0, 1,
 				 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
 				 1, 0, 0, 1, 0, 0, 5, 0, 0, 0, 5, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-				 1, 0, 2, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 1,
+				 1, 2, 2, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 1,
 				 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
 				 0, 3, 0, 0, 0, 0, 1, 4, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
 				 0, 3, 0, 0, 0, 0, 1, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
@@ -36,9 +36,9 @@ function isObstacleAtColRow(col, row) {
 	}
 }
 
-function carTrackHandling() {
-	var carTrackCol = Math.floor(carX / TRACK_W);
-	var carTrackRow = Math.floor(carY / TRACK_H);
+function carTrackHandling(whichCar) {
+	var carTrackCol = Math.floor(whichCar.x / TRACK_W);
+	var carTrackRow = Math.floor(whichCar.y / TRACK_H);
 	var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
 
 	if(carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
@@ -47,10 +47,10 @@ function carTrackHandling() {
 		if(isObstacleAtColRow( carTrackCol,carTrackRow )) {
 			// next two lines added to fix a bug, mentioned in video 9.6
 			// undoes the car movement which got it onto the wall
-			carX -= Math.cos(carAng) * carSpeed;
-			carY -= Math.sin(carAng) * carSpeed;
+			whichCar.x -= Math.cos(whichCar.ang) * whichCar.speed;
+			whichCar.y -= Math.sin(whichCar.ang) * whichCar.speed;
 
-			carSpeed *= -0.5;
+			whichCar.speed *= -0.5;
 		} // end of track found
 	} // end of valid col and row
 } // end of carTrackHandling func
